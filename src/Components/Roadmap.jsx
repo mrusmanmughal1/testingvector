d55c5c01121bef97335a4f872cac4999b4aeb833
usman <mrusmanmughal1@gmail.com>
2025-02-23 04:13:43 +0500

    neew

--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -56,7 +56,11 @@ const Roadmap = () => {
           </div>
         </div>
       </div>
-      <div className="relative w-[60%] h-96 mx-auto -top-20 border-t-1 border-cyan-300 rounded-t-[5rem] before:absolute before:top-0 before:left-0 before:w-1/2 before:h-[50%] before:border-l-1 before:border-cyan-300 before:rounded-tl-[5rem] after:absolute after:top-0 after:right-0 after:w-1/2 after:h-[50%] after:border-r-1 after:border-cyan-300 after:rounded-tr-[5rem]">
+      <div className="relative w-[60%] h-96 mx-auto -top-20 border-t border-cyan-300 rounded-t-[5rem]
+  before:content-[''] before:absolute before:top-0 before:left-0 before:w-1/2 before:h-[50%] 
+  before:border-l before:border-cyan-300 before:rounded-tl-[5rem]
+  after:content-[''] after:absolute after:top-0 after:right-0 after:w-1/2 after:h-[50%] 
+  after:border-r after:border-cyan-300 after:rounded-tr-[5rem]">
         <div className="absolute  z-40  w-96  roadbg  rounded-2xl    top-[50%] -left-56   border-cyan-300   border-2  px-10 py-10 text-white border-dotted">
           <span className="rounded-full p-2 counterbg_effect  absolute  -top-6  bg-black right-[50%] px-4 font-semibold border-blue-700 border ">
             5
